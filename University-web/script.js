7823c65ba217eff00530db5d8384c92d32434c4d
jayrk19 <84495297+jayrk19@users.noreply.github.com>
2021-07-17 20:24:54 +0530

    Add files via upload

---
+++ University-web/script.js
@@ -0,0 +1,8 @@
+let navlink = document.getElementById('js');
+let hide = () => {
+    navlink.style.right = "-200px";
+}
+let show = () => {
+    navlink.style.right = "0";
+    navlink.style.display = "block";
+}

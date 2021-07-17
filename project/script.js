ba5fb4a8c5207c6e8726ef9826f107bf4f39bf17
jayrk19 <84495297+jayrk19@users.noreply.github.com>
2021-07-17 20:16:00 +0530

    Delete project directory

--- project/script.js
+++
@@ -1,8 +0,0 @@
-let navlink = document.getElementById('js');
-let hide = () => {
-    navlink.style.right = "-200px";
-}
-let show = () => {
-    navlink.style.right = "0";
-    navlink.style.display = "block";
-}
